d5c0fa3617e38080328312fb1084347db1fc94f4
yiseul <kdewly715@gmail.com>
2025-08-13 14:41:19 +0900

    commit message입니다아아아

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	@GetMapping("/")
+	public @ResponseBody String index() {
+		return "welcome home";
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(required=false) String name,Model model) {
+		model.addAttribute("freetings","Hello~"+name);
+		return "hello";
+	}
+
+}
